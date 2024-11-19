5c97cd738dae5ef6556f30dc51426d8307c23c71
chebupa <aristarhsakulin@gmail.com>
2024-11-20 00:55:36 +0300

    initial commit

---
+++ TurtleApp-iOS/TurtleApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurtleApp_iOSApp.swift
+//  TurtleApp-iOS
+//
+//  Created by aristarh on 20.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TurtleApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TurtleApp-iOSTests/TurtleApp_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  TurtleApp_iOSTests.swift
+//  TurtleApp-iOSTests
+//
+//  Created by aristarh on 20.11.2024.
+//
+
+import Testing
+@testable import TurtleApp_iOS
+
+struct TurtleApp_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
